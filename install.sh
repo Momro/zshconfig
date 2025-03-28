1f655035478e9e1caee99f9a313477e56d28c19a
Momro <68013667+Momro@users.noreply.github.com>
2025-03-28 19:52:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "[+] Download ZSH"
 sudo apt update ; sudo apt upgrade -y ; sudo apt install zsh -y
 echo "[+] ZSH as default shell"
-sudo chsh -s $(which zsh)
+chsh -s $(which zsh) $(whoami)
 
 # download this repo
 echo "[+] Clone Momro repo"
@@ -17,7 +17,7 @@ cd $HOME ; rm .zshrc .zshrc.pre-oh-my-zsh ; ln -s $HOME/zshconfig/.zshrc $HOME/.
 
 # Requires lolcat ;) and autojump:
 echo "[+] Install lolcat and autojump"
-sudo apt install lolcat autojump
+sudo apt install lolcat autojump -y
 
 # zsh autosuggestions:
 echo "[+] Install zsh autosuggestion"
