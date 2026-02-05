7844da9dfbfaa2f1c24193d941cf537204ea4330
Momro <68013667+Momro@users.noreply.github.com>
2026-02-05 20:07:35 +0100

    Update functions.zsh

--- functions.zsh
+++ functions.zsh
@@ -5,3 +5,13 @@ function base64decode {
 function docker-health() {
   docker inspect --format='{{json .State.Health}}' "$@" | jq
 }
+
+function generate-password() {
+        if [[ $@ < 10 ]] ; then
+                pass=$(openssl rand -hex 10)
+        else
+                pass=$(openssl rand -hex $@)
+        fi
+
+        echo $pass | head -c $@ ; echo
+}
