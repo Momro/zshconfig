3fa6b561986a9cd4ffb55fd294ece333dc31abb5
Momro <68013667+Momro@users.noreply.github.com>
2026-02-05 19:57:53 +0100

    Update alias.zsh

--- alias.zsh
+++ alias.zsh
@@ -13,3 +13,10 @@ alias mv='mv -i'
 alias gitlog='git log --format="%n[Commit: %C(green)%h%Creset]  -  [Name: %C(green)%cn %C(reset)(PGP: %C(green)%G?%C(auto))]  -  [Date: %C(green)%ad%C(auto)]%d%n    %s" --date=format:"%Y-%m-%d %H:%M"'
 # grep mit color und zeilennummer
 alias grepc="grep -n --color"
+
+alias cp-large=rsync -ah --info=progress2
+alias generate-password='openssl rand -base64'
+
+alias docker-status='docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"'
+alias docker-restart='echo "restarting docker" && docker compose down && echo "docker down" && docker compose up -d && echo "docker up again" '
+
