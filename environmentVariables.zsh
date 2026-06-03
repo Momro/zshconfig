25b7c7877088b1f3ab271032d3b1bc54f37f864d
Momro <68013667+Momro@users.noreply.github.com>
2026-06-03 10:09:18 +0200

    Update environmentVariables.zsh

--- environmentVariables.zsh
+++ environmentVariables.zsh
@@ -6,6 +6,9 @@ XDG_CONFIG_HOME=~/.config/
 if [ -f "$HOME/.local/bin/gitup" ] ; then
 	PATH="$HOME/.local/bin/:$PATH"                                                       
 fi  
+# don't know why this should ever not be in PATH
+PATH="$HOME/.local/bin/:$PATH"                                                       
+
 
 # mal schauen, ob das funktioniert
 export SUDO_EDITOR="vim"
